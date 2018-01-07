33caeb5d40602fff83f1ec93821e77c2fa5ec749
Mario Corchero <mariocj89@gmail.com>
2018-01-07 12:36:39 +0000

    Generalize urls

--- eas/api.py
+++ eas/api.py
@@ -5,13 +5,32 @@ from . import models, schemas
 bp = Blueprint("api", __name__)
 
 
-def resource_create(model_class, serializer):
+_SCHEMAS = {
+    "random_number": schemas.RandomNumber,
+}
+
+_MODELS = {
+    "random_number": models.RandomNumber,
+}
+
+
+@bp.route("/<string:draw_type>", methods=["POST"])
+def post(draw_type):
+    serializer = _SCHEMAS[draw_type]()
+    model_class = _MODELS[draw_type]
+
     input_data, _ = serializer.load(request.json)
     obj = model_class.create(**input_data)
     output_data, _ = serializer.dump(obj)
     return jsonify(output_data)
 
 
-@bp.route("/random_number")
-def post():
-    return resource_create(models.RandomNumber, schemas.RandomNumber())
+@bp.route("/<string:draw_type>/<string:id_>", methods=["GET"])
+def get(draw_type, id_):
+    serializer = _SCHEMAS[draw_type]()
+    model_class = _MODELS[draw_type]
+
+    input_data, _ = serializer.load(request.json)
+    obj = model_class.query.get(id_)
+    output_data, _ = serializer.dump(obj)
+    return jsonify(output_data)
