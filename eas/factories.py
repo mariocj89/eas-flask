6afe175f5cbfb7fb5c84fda758df4394a6fa862a
Mario Corchero <mariocj89@gmail.com>
2018-03-18 20:49:20 +0000

    Ignore commands from coverage

--- eas/factories.py
+++ eas/factories.py
@@ -7,7 +7,7 @@ from . import urls
 def create_app():
     app = Flask('flaskr')
 
-    if "EAS_SETTINGS" not in os.environ:   # pragma: no cover
+    if "EAS_SETTINGS" not in os.environ:  # pragma: no cover
         raise RuntimeError(
             "Set the configuration file in the environment variable EAS_SETTINGS.\n"
             "Example: export EAS_SETTINGS=eas/settings/dev.py"
@@ -18,7 +18,7 @@ def create_app():
     register_cli(app)
     init_db(app)
 
-    if app.config.get("CREATE_DB"):
+    if app.config.get("CREATE_DB"):  # pragma: no cover
         create_db(app)
 
     return app
@@ -43,7 +43,7 @@ def init_db(app):
     from eas.models import db
     db.init_app(app)
 
-def create_db(app):
+def create_db(app):  # pragma: no cover
     from eas.models import db
     with app.app_context():
         db.create_all()
